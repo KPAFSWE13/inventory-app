f173b9c72fc017b896c8019ba7a102df8e974356
Codekaikev <codekaikev@gmail.com>
2023-03-07 15:36:28 +0000

    Kevin

--- public/react/components/SingleViewItem.js
+++ public/react/components/SingleViewItem.js
@@ -3,15 +3,23 @@ import React from 'react';
 export const SingleViewItem = ({item, setSingleItem, deleteItem}) => {
     return (
         <>
-            <h3>{item.title}</h3>
-			<h4>{item.price}</h4>
-			<p>{item.description}</p>
-			<p>{item.category}</p>
-			<img src={item.image} alt={item.title} /> 
+            <main id="singlePageViewer">
+            <br></br>
+            <br></br>
+            <h3 id="singlePageViewer">{item.title}</h3>
+            <br></br>
+			<h4 id="singlePageViewer"><strong id="singlePageViewer">Price:</strong> £{item.price}</h4>
+            
+			<p id="singlePageViewer"><strong id="singlePageViewer">Description:</strong> {item.description}</p>
+         
+			<p id="singlePageViewer"><strong id="singlePageViewer">Category:</strong> {item.category}</p>
+			<img src={item.image} alt={item.title} id=""/> 
+            <br></br>
+            <br></br>
 			<button onClick={() => setSingleItem('')}>Return Back</button>
             <button>Update Item</button>
             <button onClick={() => deleteItem()}>Delete Item</button>
-            
+            </main>
         </>
     )
 }
